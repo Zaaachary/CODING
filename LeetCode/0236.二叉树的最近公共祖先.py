a21a7b4e3f45dc4aff609f9e19eb373702248ac8
lizhifeng05 <lizhifeng05@baidu.com>
2021-12-01 22:12:18 +0800

    update new question

---
+++ LeetCode/0236.二叉树的最近公共祖先.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   0236.二叉树的最近公共祖先.py
+@Time    :   2021/11/30 15:48:03
+@Author  :   Zhifeng Li
+@Contact :   zaaachary_li@163.com
+@Desc    :   https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：
+“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，
+满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+'''
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """
+        后续遍历，找到 p 时记录堆栈祖先，找到 q 时记录堆栈祖先，通过比较堆栈找到最近祖先。
+        """
+        pass
+
