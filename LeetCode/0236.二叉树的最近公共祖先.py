4e6324c792591b11469fee0b543af67d9e411418
Zachary Li <li_zaaachary@outlook.com>
2021-12-02 23:53:48 +0800

    update tree

--- LeetCode/0236.二叉树的最近公共祖先.py
+++ LeetCode/0236.二叉树的最近公共祖先.py
@@ -13,11 +13,56 @@
 满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
 '''
 
+from typing import List
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
+    
+    @classmethod
+    def build_tree(cls, node_list:List):
+        """
+        后序遍历 node_list 遍历过程中建树
+        """
+        node_list.insert(0, None)
+        index = 1
+        node_stack = []
+        target = 1
+        while target < len(node_list):
+            node_stack.append(target)
+            target = target * 2
+        
+        last_visit = -1
+        last_visit_tree = None
+        while len(node_stack) != 0:
+            node = node_stack[-1]
+            right = node*2 + 1
+            if right < len(node_list) and node_list[right] != None and last_visit == right-1:
+                # 存在右节点且未被访问，上次访问的为该节点的左孩子
+                
+                target = right
+                while target < len(node_list):
+                    node_stack.append(target)
+                    target = target * 2
+                last_visit = -1
+                last_visit_tree = None
+
+            else:
+                # 不存在右节点 或右节点已被访问，访问当前节点
+                node_stack.pop(0)
+                value = node_list[node]
+                tree_node = cls(value)
+                if last_visit == right:
+                    # 右节点已访问
+                    tree_node.right = last_visit_tree
+                else:
+                    # 无右节点，上次访问的为该节点的左孩子
+                    tree_node.left = last_visit_tree
+                
+        
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
@@ -26,3 +71,32 @@ class Solution:
         """
         pass
 
+    @staticmethod
+    def post_traverse(root):
+        node_stack = []
+        last_visit = None
+        while root:
+            node_stack.append(root)
+            root = root.left
+
+        while len(node_stack) != 0:
+            # node 无左孩子或左孩子已经被访问
+            node = node_stack[-1]
+            if node.right and last_visit == node.left:
+                # 有右孩子，且左孩子刚被访问
+                node = node.right
+                while node:
+                    # 右孩子及其左孩子链入栈
+                    node_stack.append(node)
+                    node = node.left
+            else:
+                # 没有右孩子，或有右孩子已经被访问
+                last_visit = node_stack.pop(0)
+                print(last_visit.data)
+                
+if __name__ == "__main__":
+    TreeNode.build_tree([1,2,3])
+            
+        
+        
+        
